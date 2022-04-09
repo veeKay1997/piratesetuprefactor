ff79226df39dcb6aacb2037ebabd3417da6985a9
Tjardy van Keimpema <tjardy.van.keimpema@hva.nl>
2022-04-09 19:26:59 +0200

    reimported old stuff

--- src/main/java/nl/hva/miw/pirate_bank_setup/service/PopulateDatabaseService.java
+++ src/main/java/nl/hva/miw/pirate_bank_setup/service/PopulateDatabaseService.java
@@ -1,7 +1,6 @@
 package nl.hva.miw.pirate_bank_setup.service;
 
 import nl.hva.miw.pirate_bank_setup.domain.Customer;
-import nl.hva.miw.pirate_bank_setup.repository.RootRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
